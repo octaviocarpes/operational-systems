116d6d7c449d9b7b5d9ae8a8b9207d6c92acbe83
Octavio <octavio.carpes10@gmail.com>
2019-09-15 14:05:57 -0300

    finished first part

--- t1-opsys/cannibals.c
+++ t1-opsys/cannibals.c
@@ -9,7 +9,9 @@ typedef struct {
 } data;
 
 int TABLE;
-pthread_mutex_t lock;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int play = 0;
 data problemData = {
   .cannibals = 0,
   .meats = 0
@@ -18,44 +20,41 @@ data problemData = {
 void *
 cook_thread(void *argument) {
   int meatsToCook = *(int *)argument;
-  printf("\n\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
-  printf("I am the Cook!!! Zug Zug! \\o/\n");
-  printf("Cooking da dinner!!!\n");
-  for (int i = 1; i <= meatsToCook; i++)
-  {
-    TABLE++;
-    printf("Cooked %d pieces... (Table size: %d)\n", i, TABLE);
-    sleep(1);
+  printf("\n\n-------------------------\n\n");
+  printf("I am the Cook!!!\\o/\n");
+  printf("\n-------------------------\n\n");
+  while(1) {
+    if (TABLE == 0) {
+      pthread_mutex_lock(&lock);
+      printf("\n\n*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
+      printf("Cook: Time to Cook! \\o/\n");
+      for (int i = 1; i <= problemData.meats; i++)
+      {
+        TABLE = i;
+        printf("Cooking %d pieces of meat...(Table Size: %d)\n", i, TABLE);
+      }
+      printf("Job Done! Da table is fuul! \\o/\n");
+      printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
+      pthread_mutex_unlock(&lock);
+    }
+    sleep(3);
   }
-  printf("\n\n*JOB DONE!*\n\n");
-  printf("\n\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
-  pthread_exit(NULL);
 }
 
 void *
 cannibals_thread(void *argument)
 {
-  pthread_mutex_lock(&lock);
-
   int cannibal = *(int *)argument;
-  printf("\n\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
-  printf("I am Cannibal %d!", cannibal);
-
-  if (TABLE == 0) {
-    // wake up cook
-    printf("\nCannibal %d: COOOOK! WAKE UP! There is no FOOD! >:(\n", cannibal);
-    pthread_t my_cook;
-    pthread_create(&my_cook, NULL, cook_thread, &problemData.meats);
-    pthread_join(my_cook, NULL);
-  } else {
-    // eat from table
-    TABLE--;
-    printf("\nCannibal %d eating... (Table size: %d)\n", cannibal, TABLE);
-    printf("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
+  while(1) {
+    printf("I am Cannibal %d\n", cannibal);
+    if (TABLE > 0) {
+      printf("Cannibal: %d eats....(Table Size: %d)\n\n", cannibal, TABLE);
+      TABLE--;
+    } else if(TABLE == 0) {
+      printf("Cannibal %d: COOOK! Wake up! There is no food! >:(\n\n", cannibal);
+    }
+    sleep(2);
   }
-
-  pthread_mutex_unlock(&lock);
-  pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
@@ -70,15 +69,17 @@ int main(int argc, char *argv[]) {
   printf("Meats: %d\n", problemData.meats);
   printf("\n");
 
-  pthread_t my_cannibals[problemData.cannibals];
+  pthread_t my_cannibals[problemData.cannibals], my_cook;
 
-  while(1) {
-    for(int i = 1; i <= problemData.cannibals; i++ ) {
-      pthread_create(&my_cannibals[i], NULL, cannibals_thread, &i);
-      pthread_join(my_cannibals[i], NULL);
-      sleep(1);
-    }
+  pthread_create(&my_cook, NULL, cook_thread, &problemData.meats);
+
+  for(int i = 0; i < problemData.cannibals; i++ ) {
+    pthread_create(&my_cannibals[i], NULL, cannibals_thread, &i);
   }
+  for(int j = 0; j < problemData.cannibals; j++ ) {
+    pthread_join(my_cannibals[j], NULL);
+  }
+  pthread_join(my_cook, NULL);
 
   return 0;
 }
